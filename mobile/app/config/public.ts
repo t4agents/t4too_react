2fb569101fce7afaa3150f114950f47547011cf6
aiaccfin <aiaccfin@gmail.com>
2026-04-08 18:32:51 -0400

    remvoeapp

--- mobile/app/config/public.ts
+++
@@ -1,3 +0,0 @@
-export const SUPABASE_URL = 'https://pjenyfvefvgbldgdegxs.supabase.co';
-export const SUPABASE_ANON_KEY = 'sb_publishable_YD2rTCUgLpShbIrF1VYK3g_pEy4NR5F';
-export const API_BASE_URL = 'https://t4agents.fastapicloud.dev';
