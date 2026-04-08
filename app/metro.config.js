d4a0ea1c4757ea923cd1033169a900ec4495f9b5
aiaccfin <aiaccfin@gmail.com>
2026-04-08 12:13:00 -0400

    screenshot

--- app/metro.config.js
+++ app/metro.config.js
@@ -11,5 +11,10 @@ config.resolver.unstable_conditionNames = ['react-native', 'browser', 'default']
 if (!config.resolver.sourceExts.includes('cjs')) {
     config.resolver.sourceExts.push('cjs');
 }
+
+// Expo SDK 55 can emit a warning for this option on some Metro versions.
+if (config.watcher && Object.prototype.hasOwnProperty.call(config.watcher, 'unstable_workerThreads')) {
+    delete config.watcher.unstable_workerThreads;
+}
  
 module.exports = withNativeWind(config, { input: './global.css' })
